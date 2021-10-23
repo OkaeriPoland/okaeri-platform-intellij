6363dd03a5bad5c1c9397a60716339bd8852194d
GitHub Action <action@github.com>
2021-10-23 08:05:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pinklolicorn/okaeriplatformintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pinklolicorn.okaeriplatformintellij.services
+
+import com.github.pinklolicorn.okaeriplatformintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
